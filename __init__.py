7a27f28a19407f46d1e4043f073f0c06c8f7f5ec
lucasduffey <lucasduffey@users.noreply.github.com>
2016-09-25 10:28:30 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 # Example: https://github.com/Vector35/binaryninja-plugins/blob/master/plugins/official/sample_plugin/__init__.py
 
-# stuff goese here
-
-# this lets it be a module..
+# TODO: don't wrap like this, when binja's plugin store is more stable.
+from apkView import *
+from dexView import *
+from dexFile import *
