cb1673a6cb14a6d8a077abe07a3d6cbbb4608dde
lduffey <you@example.com>
2016-09-29 21:59:44 -0700

    reverted leb128.py - my code is bad

--- leb128.py
+++ leb128.py
@@ -7,6 +7,21 @@ def get_uleb128p1(data):
 
 	return byte_count, value
 
+# https://github.com/ondreji/dex_parser/blob/master/leb128.py
+def get_uleb128(content):
+	value = 0
+	for i in xrange(0,5):
+		tmp = ord(content[i]) & 0x7f
+		value = tmp << (i * 7) | value
+		if (ord(content[i]) & 0x80) != 0x80:
+			break
+	if i == 4 and (tmp & 0xf0) != 0:
+		print "parse a error uleb128 number"
+		return -1
+	return i+1, value
+
+'''
+# MINE: THIS MAY BE WRONG
 def get_uleb128(data):
 	total = 0
 	found = False
@@ -28,7 +43,31 @@ def get_uleb128(data):
 		assert False
 
 	return i+1, total
+'''
 
+# https://github.com/ondreji/dex_parser/blob/master/leb128.py
+def get_sleb128(content):
+	value = 0
+
+	mask=[0xffffff80,0xffffc000,0xffe00000,0xf0000000,0]
+	bitmask=[0x40,0x40,0x40,0x40,0x8]
+	value = 0
+	for i in xrange(0,5):
+		tmp = ord(content[i]) & 0x7f
+		value = tmp << (i * 7) | value
+		if (ord(content[i]) & 0x80) != 0x80:
+			if bitmask[i] & tmp:
+				value |= mask[i]
+			break
+	if i == 4 and (tmp & 0xf0) != 0:
+		print "parse a error uleb128 number"
+		return -1
+	buffer = struct.pack("I",value)
+	value, =struct.unpack("i",buffer)
+	return i+1, value
+
+'''
+# MINE: THIS MAY BE WRONG
 def get_sleb128(data):
 	total = 0
 	found = False
@@ -53,6 +92,7 @@ def get_sleb128(data):
 		assert False
 
 	return i+1, total
+'''
 
 # test cases
 assert get_sleb128("\x00")[1] == 0
