47a2c2ee14cf2a36064ee710f4f2b0e32c85c4c3
lucasduffey <lucasduffey@users.noreply.github.com>
2017-09-03 12:21:03 -0500

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,4 +1,9 @@
-# TODO
+# CTFs to train against
+# * https://github.com/xtiankisutsa/awesome-mobile-CTF
+# * TODO: need a few simple ones to benchmark
+#   * https://labs.mwrinfosecurity.com/system/assets/349/original/androidproject.apk
+#   * https://github.com/artwyman/android_ctf/tree/master/signed_apks (?)
+
 
 # check apk name - run tests and compare with historical results
 
